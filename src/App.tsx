85d624aad2b9a25277658a230e6fab8ef3e2b558
thebuilderekes <ekeopreberedugo1@gmail.com>
2025-03-30 20:44:44 +0100

    created containers and card coomponents

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { AppContainer } from "./styles/Styles.tsx";
+function App() {
+  return (
+    <>
+      <AppContainer>
+
+        columns will go here
+      </AppContainer>
+    </>
+  );
+}
+
+export default App;
